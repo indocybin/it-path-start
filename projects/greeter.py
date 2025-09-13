f27ff1150c0ad68549bf0ccc6611840822d8f97c
Jonathan Butcher <jonathanbutcher7@gmail.com>
2025-09-13 10:32:10 -0400

    Added interactive loop to greeter.py (kinda buggy tho)

--- projects/greeter.py
+++ projects/greeter.py
@@ -1,13 +1,19 @@
 from datetime import datetime
+while True:
+	name = input("What is your name? (type 'quit' to exit) ")
+	if name.lower() == "quit":
+		print ("Goodbye, see you next time!")
+		break
+
 # ASK FOR USER'S NAME
 name = input("What is your name? ")
 # GET CURRENT HOUR
 current_hour = datetime.now().hour
 # DECIDE GREETING BASED ON TIME
 if current_hour < 12:
- greeting = "Good morning"
+	greeting = "Good morning"
 elif current_hour < 18:
- greeting = "Good afternoon"
+	greeting = "Good afternoon"
 else:
- greeting = "Good evening"
-print (f"{greeting}, {name}! Welcome to your python journey.")
+	greeting = "Good evening"
+print (f"{greeting}, {name}! I'm glad you're practicing Python.")
